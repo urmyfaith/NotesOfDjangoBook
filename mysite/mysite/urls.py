2e9db24e00d08815263c7c251fd6f8650e757fcb
urmyfaith <urmyfaith@qq.com>
2014-09-13 12:33:36 +0800

    views.hours_ahead
    url模式匹配,带参数的url.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from mysite.views import hello,current_datetime
+from mysite.views import hello,current_datetime,hours_ahead
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$',hello),
     url(r'^time/$',current_datetime),
-        
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),    
 )
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 import datetime
 
 def hello(request):
@@ -8,3 +8,12 @@ def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
+    
