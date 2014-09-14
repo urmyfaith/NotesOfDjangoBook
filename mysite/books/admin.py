739c71725dbacb6e6935987cd121d4671c6827bc
urmyfaith <urmyfaith@qq.com>
2014-09-14 11:26:59 +0800

    管理界面-表-显示字段设置,
    list_display=('first_name','last_name','email')

--- mysite/books/admin.py
+++ mysite/books/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from books.models import Author,Publisher,Book
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=('first_name','last_name','email')
+
 
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author,AuthorAdmin)
 admin.site.register(Book)
